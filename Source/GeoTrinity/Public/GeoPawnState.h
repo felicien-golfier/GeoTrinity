f4ab7a253949e42c84f9bac6a115d23b0c6296af
felicien-golfier <feloux@gmail.com>
2025-10-27 15:39:31 +0100

    Add firt RollBack made by Juny ;)

---
+++ Source/GeoTrinity/Public/GeoPawnState.h
@@ -0,0 +1,37 @@
+﻿// Copyright (c) GeoTrinity
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputStep.h"
+
+class AGeoPawn;
+// Lightweight snapshot of a character's state for saving/serialization/logging.
+struct FGeoPawnState
+{
+	TWeakObjectPtr<AGeoPawn> GeoPawn;
+
+	// World-space location of the GeoPawn (cm)
+	FVector Location = FVector::ZeroVector;
+
+	// World-space orientation of the character
+	FRotator Orientation = FRotator::ZeroRotator;
+
+	// Current velocity (cm/s)
+	FVector Velocity = FVector::ZeroVector;
+
+	FGeoPawnState() = default;
+
+	FGeoPawnState(AGeoPawn* InGeoPawn, const FVector& InLocation, const FRotator& InOrientation, const FVector& InVelocity)
+		: GeoPawn(InGeoPawn)
+		, Location(InLocation)
+		, Orientation(InOrientation)
+		, Velocity(InVelocity)
+	{
+	}
+};
+
+struct FGeoGameSnapShot
+{
+	FGeoTime Time;
+	TArray<FGeoPawnState> GeoPawnStates;
+};
---
+++ Source/GeoTrinity/Public/GeoRestoreGameInstanceSubsystem.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GeoPawnState.h"
+#include "GeoRestoreGameInstanceSubsystem.generated.h"
+
+UCLASS()
+class GEOTRINITY_API UGeoRestoreGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	// Apply a previously captured snapshot to restore pawns to a specific game state.
+	void ApplySnapshot(const FGeoGameSnapShot& Snapshot);
+
+	// Helper to fetch the subsystem instance for a given world.
+	static UGeoRestoreGameInstanceSubsystem* GetInstance(UWorld* World);
+};
